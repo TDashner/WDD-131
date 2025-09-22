9715ed75e5aa4a93b4370b2babe66a05443ab374
TDashner <das21007@byui.edu>
2025-09-22 14:24:15 -0600

    debugging

---
+++ mission_statement/debugging.js
@@ -0,0 +1,17 @@
+// const pi = 3.14;
+// let radius = 3;
+// let area = 0;
+// area = radius * radius * pi;
+// console.log("A1", area)
+// radius = 4;
+// area = radius * radius * pi;
+// console.log("A2", area)
+
+const PI = 3.14;
+let area = 0;
+function circleArea(radius) {
+  const area = radius *radius * PI;
+  return area;
+}
+area = circleArea(3);
+console.log(area);
